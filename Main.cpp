1237c7d1683df3befe088f0ef21faec668cc49f5
Cristian Javier Arraya Quispe <arrayacristian75@gmail.com>
2025-03-04 17:20:47 -0300

    Operador New

--- Main.cpp
+++ Main.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int * punteroA;//Declarar el puntero que guardara la direccion
-    int variableEnt = 10;//Una variable que se inicia con 10
+    int *pA;      // Declaramos un puntero
+    pA = new int; // solicitamos memoria del operador "new" para el numero entero
+    *pA = 21;     // Modificamos el valor entero al cual apuntamos
 
-    punteroA = &variableEnt;//el "&" significa "direccion de" y el nombre de la variable que queremos saber su direccion
-    *punteroA = 31;//Cambiar el valor de la variable NO del puntero mediante el "*"
-    cout << variableEnt;
+    cout << "Valor de la variable: " << *pA << endl;
+    cout << "Direccion de la memoria: " << &pA << endl;
     return 0;
 }
