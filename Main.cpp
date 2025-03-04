4659e85d5dd2e9553868b6e1d465c7102e9667c7
Cristian Javier Arraya Quispe <arrayacristian75@gmail.com>
2025-03-04 18:58:00 -0300

    Ejemplo con puntero y uso de DELETE para liberar espacio

--- Main.cpp
+++ Main.cpp
@@ -1,33 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*Usualmente al enviar un parámetro a una función todo lo que se haga con dicho parámetro allí adentro NO tiene efecto por fuera. 
-Por ejemplo si a una función la se le envía una variable cuyo valor es diez y al interior de la función le sumamos un cinco, 
-después de la ejecución de la función el valor de la variable seguirá siendo diez en vez de quince. 
-Lo que pasó al interior de la función se quedó allí. Para solucionar esto, si queremos que el valor cambie definitivamente, 
-usamos punteros para pasar no el valor del parámetro sino una referencia a éste */
-
-int funcion(int valor)
-{
-    valor += 10;//Se le suma 10
-    return valor;
-}
-
-int funcionPunteros(int* valor)
-{
-    *valor += 10;//Se le suma 10 a la posicion
-    return *valor;
-}
 int main(int argc, char const *argv[])
 {
-    int numero = 10;
-    cout << "Antes de funcion " << numero << "\n";//10
-    funcion(numero);//Se pasa por valor
+    string* titulos = NULL;
+    string* autores = NULL;
+    int tamanio;
+    cout << "Cuantos libros desea ingresar?" << endl;
     
-    cout << "Despues de funcion " << numero << "\n";//10
-    cout << "Antes de funcionPunteros " << numero << "\n";//10
+    string entrada;
+    getline(cin, entrada);
+    tamanio = stoi(entrada);
+
+    titulos = new string[tamanio];//El new es como guardar espacio para esa variable
+    autores = new string[tamanio];
     
-    funcionPunteros(&numero);//Se envia la direccion de la memoria y la funcion resuelve la referencia 
-    cout << "Despues de funcion Punteros " << numero << "\n";//20 (10 + 10)
+    cout << "Por favor ingrese la siguiente informacion de los libros: \n";
+    for(int i = 0; i < tamanio; i++)
+    {
+        cout << "\n****** Libro " << i + 1 << "******\n";
+        cout << "Titulo: ";
+        getline(cin, titulos[i]);
+        cout << "Autor: ";
+        getline(cin, autores[i]);
+    }
+    delete[] titulos;//Una vez usado borramos los datos para liberar espacio
+    delete[] autores;
+    titulos = NULL;
+    autores = NULL;
     return 0;
 }
