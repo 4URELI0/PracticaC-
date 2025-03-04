a5a626e193aaf613dc6b359f0908fbbc1beb8fec
Cristian Javier Arraya Quispe <arrayacristian75@gmail.com>
2025-03-04 10:19:48 -0300

    Cambiar el valor de la varible que se apunta

--- Main.cpp
+++ Main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char const *argv[])
     int variableEnt = 10;//Una variable que se inicia con 10
 
     punteroA = &variableEnt;//el "&" significa "direccion de" y el nombre de la variable que queremos saber su direccion
-    cout << punteroA;
+    *punteroA = 31;//Cambiar el valor de la variable NO del puntero mediante el "*"
+    cout << variableEnt;
     return 0;
 }
