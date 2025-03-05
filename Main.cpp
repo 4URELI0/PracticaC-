da00d68a6a3f9422ea4d9c4d8cbe06c2337a38de
Cristian Javier Arraya Quispe <arrayacristian75@gmail.com>
2025-03-05 16:22:55 -0300

    El uso de * y del & en arrays

--- Main.cpp
+++ Main.cpp
@@ -3,32 +3,13 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    /*int aiNotas[10];//Array de numeros enteros
-    int * pINotas;//Puntero para el array *Debe ser del mismo tipo de dato osea un int
+    int* piNotas = new int[1];
+    piNotas[0] = 10;
 
-    //int * pINotas = new int;//Cuando queremos apuntar a un solo numero entero
-    int * pINotas = new int[10];//Cuando queremos apuntar a un array */
+    cout << "Direccion de piNotas: " << piNotas << endl;
+    cout << "Direccion de &piNotas[0]: " << &piNotas[0] << endl;
+    cout << "Valor de *piNotas: " << *piNotas << endl;
 
-    unsigned short int tamArray;
-    
-    cout << "Ingrese el tamanio del array: " << endl;
-    cin >> tamArray;
-    cout << endl;
-
-    int * piNotas = new int[tamArray];
-    
-    /*Verificamos que la memoria haya sido asignada, en caso que no lo fuera apuntaria a 0*/
-    if (piNotas)
-    {
-        long iSumaNotas = 0;
-        for (int i = 0; i < tamArray; i++)
-        {
-            cout << endl << "Ingrese nota " << i + 1 << ": ";
-            cin >> piNotas[i];
-            iSumaNotas += piNotas[i];
-        }
-        cout << "El promedio de notas es: " << iSumaNotas / (float) tamArray << endl;
-        delete[] piNotas;
-    }
+    delete[] piNotas;
     return 0;
 }
