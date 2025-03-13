91641b2191cdd3ed0cf8a85d4a4e85603823d54f
Cristian Javier Arraya Quispe <arrayacristian75@gmail.com>
2025-03-13 19:22:39 -0300

    Se uso la referencia, es casi igual que los punteros pero menos poderoso

--- Main.cpp
+++ Main.cpp
@@ -3,17 +3,14 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    char * pszDecir = new char[64];
-    strcpy(pszDecir, "Que sea paranoico no signfica que me esten siguiendo");
-    char * pszCursor = pszDecir;//Se crea un puntero para poder desplazarnos por el original
-    
-    /*Recorremos el array, incrementando la posicion del puntero*/
-    while (*pszCursor)
-    {
-        cout << *pszCursor;
-        pszCursor++;
-    }
+    //Una variable
+    int iVal = 10;
+    //Una referencia de la variable anterior
+    int &ref = iVal;
 
-    delete[] pszDecir;
+    /*Modifica la referencia "ref" que es como modificar la variable iVal*/
+    ref = 20;
+
+    cout << "El valor de ref iVal es: " << iVal << endl;
     return 0;
 }
