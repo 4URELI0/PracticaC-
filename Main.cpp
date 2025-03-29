c1758b7588896b184ad0aec03c31948c58c49699
Cristian Javier Arraya Quispe <arrayacristian75@gmail.com>
2025-03-29 19:59:00 -0300

    Operador posIncremento

--- Main.cpp
+++ Main.cpp
@@ -1,25 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void Food(int *a)
-{
-    *a = 20;
-}
-void Food2(int b)
-{
-    b = 30;
-}
 int main(int argc, char const *argv[])
 {
-    int iVar = 10;
-    int iVar2 = 40;
-    //Paso la variable iVar por el Valor
-    Food(&iVar);
-    //iVar fue modificado
-    cout << iVar << endl;
+    /*FUNCION POSINCREMENTO*/
+    //Primero se realiza la asignacion y luego el incremento
+    int a = 1;
+    int b = a++;//Primero se asigna la variable a 1 y luego incrementa en a
 
-    /*iVar2 no fue modificado*/
-    Food2(iVar2);
-    cout << iVar2 << endl;
+    cout <<"El valor de a: "<< a << endl;
+    cout <<"El valor de b: "<< b << endl;
     return 0;
 }
